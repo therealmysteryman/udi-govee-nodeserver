19a0b11a9e66fcc7c73aee96c2ef613faf2b8a03
automationgeek <jf.tremblay@gmail.com>
2021-01-19 14:24:28 -0500

    Update commitToGit.sh

--- commitToGit.sh
+++ commitToGit.sh
@@ -3,5 +3,5 @@ git commit -m "Travis build: $TRAVIS_BUILD_NUMBER [skip ci]"
 git branch tmp_$TRAVIS_BRANCH
 git checkout $TRAVIS_BRANCH
 git merge tmp_$TRAVIS_BRANCH
-git remote add origin-pages https://$GITHUB_TOKEN@github.com/therealmysteryman/udi-twinkly-nodeserver
+git remote add origin-pages https://$GITHUB_TOKEN@github.com/therealmysteryman/udi-govee-nodeserver
 git push origin-pages $TRAVIS_BRANCH 
